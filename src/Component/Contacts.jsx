f1a337188ee16b2316f6c42befdbfdb06d73f8c4
SabirMehdiyev <sabirsm@code.edu.az>
2023-11-17 17:32:26 +0400

    initial

---
+++ src/Component/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contacts = () => {
+    return (
+        <div>
+            <p>Phone : +994558528134</p>
+            <p>Email : sabirmextiev05@gmail.com</p>
+        </div>
+    )
+}
+
+export default Contacts
---
+++ src/Component/PersonalInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+  return (
+    <div>
+    <p>Name : Sabir</p>
+    <p>Surname : Mehdiyev</p>
+    <p>Age : 20</p>    
+    <p>Address :Baku, Hesenbey Zerdabi 3166</p>
+    </div>
+  )
+}
+
+export default PersonalInfo
